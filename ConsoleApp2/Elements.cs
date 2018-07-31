68ee915a0cbe71951ae9c74e9386be22e96686ed
Данил Манмарёв <danr428@gmail.com>
2018-07-31 11:11:17 +0300

    Попытка продебажить кнопку

---
+++ ConsoleApp2/Elements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp2
+{
+    
+    static class Elements
+    {
+        static IWebDriver driver = Program.driver;
+
+            public static String RegistrationButtonLocator = "body > div.wrapper > div > div.header-main.closed > div > div.header-area-sticky.no-transition > div.header-main-area > div > div.header-area-left > div.header-login > ul > li:nth-child(2) > a";
+        public static IWebElement RegstrationButton = driver.FindElement(By.CssSelector(RegistrationButtonLocator));
+
+            public static String RegionApproveButtonLocator = "#tooltipster-15729 > div.tooltipster-box > div > div > div > a.btn.btn-approve-city";
+        public static IWebElement RegionApproveButton = driver.FindElement(By.CssSelector(RegionApproveButtonLocator));
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static IWebElement WaitUntilElementExists(By elementLocator, int timeout = 100)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementExists(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                return driver.FindElement(elementLocator);
+            }
+        }
+        public static IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+            driver.Navigate().GoToUrl("https://www.mvideo.ru/");
+            driver.Manage().Window.Maximize();
+            //driver.Manage().Timeouts().ImplicitWait;
+            System.Threading.Thread.Sleep(5000);
+            // var waitRegion = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // waitRegion.Until(ExpectedConditions.ElementExists(By.XPath(Elements.RegistrationButtonLocator)));
+
+            // WaitUntilElementExists(By.CssSelector(Elements.RegionApproveButtonLocator));
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                     wait.Until(ExpectedConditions.ElementExists(By.CssSelector(Elements.RegionApproveButtonLocator)));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Element with locator: '" + Elements.RegionApproveButtonLocator + "' was not found in current context page.");
+                    // driver.FindElement(elementLocator);
+            }
+            Elements.RegionApproveButton.Click();
+
+               // var waitRegistration = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                //waitRegistration.Until(ExpectedConditions.ElementExists(By.CssSelector(Elements.RegistrationButtonLocator)));
+
+
+            Elements.RegstrationButton.Click();
+
+        }
+    }
+}
